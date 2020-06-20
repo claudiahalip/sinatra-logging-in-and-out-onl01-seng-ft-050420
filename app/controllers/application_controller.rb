08f380d071ca9e88ac55d1a6ee8b058953be146e
claudia v <claudia4ro@yahoo.com>
2020-06-20 22:32:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params[:username])
     if @user
-      
-    session[user_id] = @user.id
+      session[:user_id] = @user.id
+      redirect 
 
   end
 
