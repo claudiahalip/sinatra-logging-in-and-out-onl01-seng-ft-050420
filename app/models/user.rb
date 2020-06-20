bbdceda5eb156fd3644cbf2c428614c5427a7b72
claudia v <claudia4ro@yahoo.com>
2020-06-20 22:57:41 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ActiveRecord::Base 
   
+  def self.current_user(session)
+    @user_found = User.find_by(session[:user_id])
+    if @user_found
+      @user_found
+    end 
+  end 
+  
+  def self.is_logged_in?(session)
+    if !!session.include?(session[:user_id])
+      true 
+    else
+      false 
+    end 
+    
+  end 
+  
 end 
 
