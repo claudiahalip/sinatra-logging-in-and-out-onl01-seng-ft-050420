b800de55d5626bf7b7ab327e05a540a68db81e4f
claudia v <claudia4ro@yahoo.com>
2020-06-20 23:03:24 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,9 +2,7 @@ class Helpers
   
   def self.current_user(session)
     @user_found = User.find_by(session[:user_id])
-    if @user_found
-      @user_found
-    end 
+    
   end 
   
   def self.is_logged_in?(session)
