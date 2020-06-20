d231e10698e681581ef3cb8cc9f38259db965585
claudia v <claudia4ro@yahoo.com>
2020-06-20 22:31:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    user = User.find_by(username: params[:username], password: params[:password])
-    if 
+    @user = User.find_by(username: params[:username])
+    if @user
+      
     session[user_id] = user[:id]
 
   end
